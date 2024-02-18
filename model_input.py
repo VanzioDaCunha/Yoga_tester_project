5c181a58a06d1638ef10dc1b405b7d3393348fd5
divya <divyakale84@gmail.com>
2024-02-18 17:22:05 +0530

    data preprocessing

---
+++ model_input.py
@@ -0,0 +1,47 @@
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+
+# Function takes csv file as an input and returns 2 Numpy arrays
+
+def data_preprocessing(filename):
+    arr = []
+    # Using loadtxt() to write data from csv file to arr list
+    arr = np.loadtxt(filename, delimiter=",", dtype=str)
+
+    # X_list to store current time along with keypoints
+    # Y_list to store the activity corresponding to the current time
+    X_list = []
+    Y_list = []
+    for data in arr:
+        line = []
+        flag = 0
+        for i, value in enumerate(data):
+            if value == '':
+                flag = 1
+                break
+            elif i == 133:
+                Y_list.append(value)
+            else:
+                line.append(value)
+        if flag == 0:
+            X_list.append(line)
+
+    print(len(X_list))
+    print(len(Y_list))
+
+    # converting list to Numpy array
+    x = np.array(X_list)
+    y = np.array(Y_list)
+
+    return x, y
+
+# Use LabelEncoder to convert string labels to integers
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(y)
+
+y = to_categorical(y)
+print(y)
+
+# Inspect the mapping between original labels and encoded labels
+print("class labels : ", label_encoder.classes_)
