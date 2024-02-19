e814ad4d7aca9dbcfcdf8b54b0208a589486fc2d
divya <divyakale84@gmail.com>
2024-02-19 20:57:54 +0530

    added labels constant

--- constants.py
+++ constants.py
@@ -3,3 +3,5 @@
 MODEL_INPUT = 133
 CLASS_OUTPUT = 9
 SEQUENCE_LENGTH = 4
+LABELS = {'treepose_start', 'tp_inter1', 'tp_inter2', 'tp_inter3', 'tp_final',
+        'tp_inter4', 'tp_inter5', 'tp_inter6', 'tp_end'}
