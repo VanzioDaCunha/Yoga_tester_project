93f809b170e3a1f5f8b0aee94cfaeb5e200c1c22
Vanzio Da cunha <skydiverxv@gmail.com>
2024-03-23 17:55:03 +0530

    Fixed Comment in some places

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+NEW_VIDEO_COUNT: int = 10
+
 """
 # define all constants FOR Trikonasana
 MODEL_INPUT = 99
@@ -20,6 +22,7 @@ MODEL_LINK = 'Models/' + ASANA + '.keras'
 TF_MODEL_LINK = 'Models/' + ASANA + '.tflite'
 """
 
+"""
 
 # defined all the constants for Vrikshasana
 MODEL_INPUT: int = 99
@@ -41,7 +44,7 @@ OUTPUT_FILE = 'output' + NUMBER + '.csv'
 MODEL_LINK = 'Models/' + ASANA + '.keras'
 TF_MODEL_LINK = 'Models/' + ASANA + '.tflite'
 
-
+"""
 
 """
 # defined all the constants for Pavana_Muktasana
@@ -65,7 +68,6 @@ MODEL_LINK = 'Models/' + ASANA + '.keras'
 TF_MODEL_LINK = 'Models/' + ASANA + '.tflite'
 
 """
-
 
 """
 # defined all the constants for Tadasana
@@ -111,3 +113,23 @@ MODEL_LINK = 'Models/' + ASANA + '.keras'
 TF_MODEL_LINK = 'Models/' + ASANA + '.tflite'
 
 """
+
+# defined all the constants for Ardha_Ustrasana
+MODEL_INPUT: int = 99
+CLASS_OUTPUT: int = 5
+SEQUENCE_LENGTH: int = 6
+NUMBER = '5'
+ASANA = 'Ardha_Ustrasana'
+LABELS = ['ardhau_final', 'ardhau_start', 'inter1', 'inter2', 'inter3']
+
+CSV_FILE_PATH = 'Dataset/Yoga/' + ASANA + '/Annotations/'
+CSV_FILE = NUMBER + '.csv'
+
+VIDEO_FILE_PATH = 'Dataset/Yoga/' + ASANA + '/Videos/'
+VIDEO_FILE = NUMBER + '.mp4'
+
+OUTPUT_FILE_PATH = 'Dataset/Yoga/' + ASANA + '/Output/'
+OUTPUT_FILE = 'output' + NUMBER + '.csv'
+
+MODEL_LINK = 'Models/' + ASANA + '.keras'
+TF_MODEL_LINK = 'Models/' + ASANA + '.tflite'
